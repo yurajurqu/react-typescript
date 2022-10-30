a045aaf2c529fc03e0ecbdf0cfbca871e7b3db2b
omar.barboza <omar.barboza.globant@kantar.com>
2022-10-29 22:07:44 -0500

    fxnal component

--- src/index.tsx
+++ src/index.tsx
@@ -4,32 +4,34 @@ import ReactDOM from 'react-dom/client';
 interface AppProps {
     color?: string;
 }
-
-
-class App extends React.Component<AppProps> {
-    state = {
-        counter: 0
-    }
-   
-
-    onUp = (): void =>{
-        this.setState({counter: this.state.counter+1})
-    }
-    onDown = (): void =>{
-        this.setState({counter: this.state.counter-1})
-    }
-
-    render(){
-        return (
-            <div>
-                <button onClick={this.onUp}>+</button>
-                {this.state.counter}
-                <button onClick={this.onDown}>-</button>
-            </div>
-        )
-    }
+const App = (props: AppProps) : JSX.Element => {
+    return (<div>{props.color}</div>)
 }
 
+// class App extends React.Component<AppProps> {
+//     state = {
+//         counter: 0
+//     }
+   
+
+//     onUp = (): void =>{
+//         this.setState({counter: this.state.counter+1})
+//     }
+//     onDown = (): void =>{
+//         this.setState({counter: this.state.counter-1})
+//     }
+
+//     render(){
+//         return (
+//             <div>
+//                 <button onClick={this.onUp}>+</button>
+//                 {this.state.counter}
+//                 <button onClick={this.onDown}>-</button>
+//             </div>
+//         )
+//     }
+// }
+
 ReactDOM.createRoot(document.getElementById('root')!)
-.render(<App ></App>);
-// .render(<App color="green"></App>);
+// .render(<App ></App>);
+.render(<App color="green"></App>);
